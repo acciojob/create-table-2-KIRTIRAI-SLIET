7e0407142b5ad18dfbd1564e664986f836c4d1ff
KIRTI-GITHUB <33566404+KIRTIRAI-SLIET@users.noreply.github.com>
2026-04-06 15:33:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
 function createTable() {
     //Write your code here
-  
+    const rn = prompt("Input number of rows");
+    const cn = prompt("Input number of columns");
+
+    const rows = parseInt(rn);
+    const cols = parseInt(cn);
+
+    if (isNaN(rows) || isNaN(cols)) {
+        return;
+    }
+
+    if (rows <= 0 || cols <= 0) {
+        alert("Please enter positive numbers greater than 0");
+        return;
+    }
+
+    const table = document.getElementById("myTable");
+
+    table.innerHTML = "";
+
+    for (let i = 0; i < rows; i++) {
+        const tr = document.createElement("tr");
+
+        for (let j = 0; j < cols; j++) {
+            const td = document.createElement("td");
+            td.textContent = `Row-${i} Column-${j}`;
+            tr.appendChild(td);
+        }
+
+        table.appendChild(tr);
+    }
 }
